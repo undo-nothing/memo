1ac55af815cd2b8649172e82e2f985cc34166d9c
damien.yang <damien.yang@zkteco.com>
2021-03-09 17:41:32 +0800

    update config

--- .umirc.ts
+++ .umirc.ts
@@ -4,15 +4,19 @@ const repo = 'memo';
 
 export default defineConfig({
   title: repo,
-  favicon: '/images/icon.png',
-  logo: '/images/icon.png',
+  favicon: './images/icon.png',
+  logo: './images/icon.png',
   outputPath: 'docs-dist',
   locales: [
     ['zh-CN', '中文'],
     ['en-US', 'English'],
   ],
   mode: 'site',
+  history:
+    process.env.NODE_ENV === 'production'
+      ? { type: 'hash' }
+      : { type: 'browser' },
   hash: false,
-  base: '/',
-  publicPath: '/',
+  base: process.env.NODE_ENV === 'production' ? '/memo/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/memo/' : '/',
 });
